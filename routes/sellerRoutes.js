fca78697fa5388d8989a5383332ce5c65093a9b9
abhishek0405 <abhishekanantharam123@gmail.com>
2021-03-27 17:37:48 +0530

    seller product integration done

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -5,8 +5,19 @@ const Seller = require('../models/seller');
 const isLoggedIn = require('../middleware/Auth/isLoggedin');
 const isSeller = require('../middleware/Auth/isSeller');
 router.get('/home',isLoggedIn,isSeller,(req,res)=>{
+    Seller.findById(req.userData.id)
+          .populate('myProducts')
+          .exec()
+          .then(foundSeller=>{
+              console.log("the seller found is ");
+              console.log(foundSeller);
+            res.render('Seller/sellerHome',{userData:req.userData,myProducts:foundSeller.myProducts});
+          })
+          .catch(err=>{
+              console.log(err);
+              res.send("Unexpected err");
+          })
     
-    res.render('Seller/sellerHome',{userData:req.userData});
 })
 
 
