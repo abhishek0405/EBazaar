3649cfefa2788a2e740c662cc89bb0fd7297462b
nandand26 <nandandeshpande5@gmail.com>
2021-06-01 02:16:04 +0530

    Co-authored-by: Rohan Nolan Lasrado <lasradorohan@users.noreply.github.com>

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -5,20 +5,25 @@ const Seller = require('../models/seller');
 const isLoggedIn = require('../middleware/Auth/isLoggedin');
 const isSeller = require('../middleware/Auth/isSeller');
 const isLoggedin = require('../middleware/Auth/isLoggedin');
+
+
 router.get('/home',isLoggedIn,isSeller,(req,res)=>{
+    res.render('Seller/sellerHome')
+})
+
+router.get('/products',isLoggedIn,isSeller,(req,res)=>{
     Seller.findById(req.userData.id)
           .populate('myProducts')
           .exec()
           .then(foundSeller=>{
               console.log("the seller found is ");
               console.log(foundSeller);
-            res.render('Seller/sellerHome',{userData:req.userData,myProducts:foundSeller.myProducts});
+            res.render('Seller/products',{userData:req.userData,myProducts:foundSeller.myProducts});
           })
           .catch(err=>{
               console.log(err);
               res.send("Unexpected err");
           })
-    
 })
 
 router.get('/orders',isLoggedin,isSeller,(req,res)=>{
@@ -27,6 +32,9 @@ router.get('/orders',isLoggedin,isSeller,(req,res)=>{
         .populate(' myOrders.products myOrders.customer')
         .exec()
         .then(foundSeller=>{
+          // foundSeller.myOrders.forEach(element => {
+          //   console.log(element.products)
+          // });
           res.render("Seller/orders.ejs",{orders:foundSeller.myOrders.reverse()})
         })
         .catch(Err=>{
